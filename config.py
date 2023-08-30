0fecdc6ed58503445997a5519153d383bcca6195
deadboy2537@gmail.com <deadboy2537@gmail.com>
2023-08-30 23:10:24 +0800

    新增可配置项gpt_2_api

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ from pydantic import BaseModel, Extra
 
 
 class Config(BaseModel, extra=Extra.ignore):
-    gpt_2_api: str = "http://127.0.0.1:5000/"
+    gpt_2_api: str = "http://127.0.0.1:5000"
 
 config = Config.parse_obj(get_driver().config)
